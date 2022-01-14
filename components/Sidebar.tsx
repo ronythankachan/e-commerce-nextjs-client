ab9e16ca9494b16edaa9338a7aae8e7aadcf5a50
rony thankachan <rony.mail2me@gmail.com>
2022-01-15 04:48:20 +0530

    basic sidebar navigation

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,18 +4,18 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
-const Sidebar = () => {
+const Sidebar = (props: { setTab: any }) => {
   return (
     <div className="w-fit h-screen bg-white shadow-md py-2">
-      <div className="nav-btn m-2">
+      <div className="nav-btn m-2" onClick={() => props.setTab("dashboard")}>
         <HomeIcon className="h-5 w-5" />
         <p className="text-sm pr-12 hover:pr-12">Dashboard</p>
       </div>
-      <div className="nav-btn m-2">
+      <div className="nav-btn m-2" onClick={() => props.setTab("products")}>
         <ShoppingBagIcon className="h-5 w-5" />
         <p className="text-sm pr-12 hover:pr-12">Products</p>
       </div>
-      <div className="nav-btn m-2">
+      <div className="nav-btn m-2" onClick={() => props.setTab("orders")}>
         <ShoppingCartIcon className="h-5 w-5" />
         <p className="text-sm pr-12 hover:pr-12">Orders</p>
       </div>
---
+++ pages/admin.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { useState } from "react";
+import Dashboard from "../components/Dashboard";
+import Navbar from "../components/Navbar";
+import Orders from "../components/Orders";
+import Products from "../components/Products";
+import Sidebar from "../components/Sidebar";
+
+const admin = () => {
+  const [tab, setTab] = useState<String>("dashboard");
+  const openedTab = (tab: String) => {
+    switch (tab) {
+      case "dashboard":
+        return <Dashboard />;
+      case "products":
+        return <Products />;
+      case "orders":
+        return <Orders />;
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex">
+        <Sidebar setTab={setTab} />
+        {openedTab(tab)}
+      </main>
+    </div>
+  );
+};
+
+export default admin;
