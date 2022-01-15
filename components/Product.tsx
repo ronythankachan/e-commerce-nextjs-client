c9dac7048b0b3ca498f42e946e1482cd223e3944
rony thankachan <rony.mail2me@gmail.com>
2022-01-15 15:33:22 +0530

    product design complete

--- components/Product.tsx
+++ components/Product.tsx
@@ -6,19 +6,19 @@ const currencySymbols: any = {
 };
 const Product = (props: { product: any }) => {
   return (
-    <div className="m-4 border-2 p-2 rounded-md">
+    <div className="m-4 border-2 p-2 rounded-md space-y-2">
       <Image width={170} height={180} src={props.product.img} />
       <p className="text-sm text-gray-500 mt-2">{props.product.title}</p>
       <p className="text-sm font-semibold">
         {currencySymbols[props.product.price.currency] +
           props.product.price.value}
       </p>
-      <div className="flex justify-evenly space-x-2 mt-3">
-        <button className="w-1/2 text-xs py-1 ring-1 ring-gray-300 flex justify-center items-center gap-x-2 rounded text-gray-500 hover:cursor-pointer hover:ring-black transition-all duration-150 ease-in-out">
+      <div className="flex space-x-2">
+        <button className="w-1/2 text-xs py-1 px-2 border border-gray-300 flex justify-evenly items-center rounded text-gray-500 hover:cursor-pointer hover:border-black transition-all duration-150 ease-in-out">
           <PencilIcon className="w-4 h-4" />
           Edit
         </button>
-        <button className="w-1/2 text-xs py-1 ring-1 ring-gray-300 flex justify-center items-center gap-x-2 rounded text-red-500 hover:cursor-pointer hover:text-white hover:bg-red-500 transition-all duration-150 ease-in-out">
+        <button className="w-1/2 text-xs py-1 px-2 border border-gray-300 flex justify-evenly items-center rounded text-red-500 hover:cursor-pointer hover:text-white hover:bg-red-500 hover:border-none transition-all duration-150 ease-in-out">
           <TrashIcon className="w-4 h-4" />
           Delete
         </button>
