52b9315b7989fe54cccae3a4e01e5c32fe78bd01
rony thankachan <rony.mail2me@gmail.com>
2022-01-16 18:34:09 +0530

    dashboard summary added

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,9 +1,38 @@
-const Dashboard = () => {
-    return (
-        <div className="bg-gray-50 w-full p-5 md:p-10 ml-16 md:ml-60">
-            dashboard
-        </div>
-    )
-}
+import {
+  CurrencyRupeeIcon,
+  ShoppingBagIcon,
+  TruckIcon,
+} from "@heroicons/react/outline";
 
-export default Dashboard
+const Dashboard = () => {
+  return (
+    <div className="bg-gray-50 w-full p-5 md:p-10 ml-16 md:ml-60 space-y-4">
+      <h1 className="text-4xl font-bold justify-start">Dashboard</h1>
+      <div className="grid grid-cols-3 gap-8">
+        <div className="bg-white shadow-md rounded h-20 px-10 flex  items-center border space-x-8">
+          <CurrencyRupeeIcon className="h-12 w-12 text-green-600 p-2 bg-green-300 rounded-full" />
+          <div>
+            <h3>Total Sales</h3>
+            <p>Rs. 123000</p>
+          </div>
+        </div>
+        <div className="bg-white shadow-md rounded h-20 px-10 flex items-center border space-x-8">
+          <TruckIcon className="h-12 w-12 text-blue-600 p-2 bg-blue-300 rounded-full" />
+          <div>
+            <h3>Total Orders</h3>
+            <p>4353</p>
+          </div>
+        </div>
+        <div className="bg-white shadow-md rounded h-20 px-10 flex  items-center border space-x-8">
+          <ShoppingBagIcon className="h-12 w-12 text-yellow-600 p-2 bg-yellow-300 rounded-full" />
+          <div>
+            <h3>Total products</h3>
+            <p>47395</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
