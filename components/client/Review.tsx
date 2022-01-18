8abf173d7e50dab2c5bfe3a3df1db61431cc11a5
rony thankachan <rony.mail2me@gmail.com>
2022-01-19 03:58:17 +0530

    ugly review

--- components/client/Review.tsx
+++ components/client/Review.tsx
@@ -1,7 +1,16 @@
+import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
+
 const Review = (params: { review: any }) => {
   return (
-    <div>
-      <p></p>
+    <div className="flex">
+      <div className="flex flex-col items-center px-2 space-y-2">
+          <small className="flex text-sm items-center p-1 px-2 rounded-md bg-gray-200">{params.review.rating}<StarIcon className="w-5 h-5"/></small>
+          <HeartIcon className="w-6 h-6"/>
+      </div>
+      <div className="w-full border min-h-[100px] rounded-md p-4">
+          <p className="text-gray-500 text-sm">{params.review.review}</p>
+      </div>
     </div>
   );
 };
