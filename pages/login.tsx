3486721235c34ff229cfb9765f9972b5130b6503
rony thankachan <rony.mail2me@gmail.com>
2022-01-19 18:48:44 +0530

    login and signup page added for client

---
+++ pages/login.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Head from "next/head";
+import { useState } from "react";
+
+const login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <div className="md:h-screen w-screen flex justify-center items-center">
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-96 bg-white md:shadow-md rounded-md p-6 space-y-10 m-2">
+        <h1 className="font-bold text-4xl text-center">Login</h1>
+        <form className="flex flex-col space-y-4">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Enter e-mail"
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            className="input-text"
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            onChange={handleChange}
+          />
+          <button className="btn-primary" type="submit" onClick={login}>
+            Login
+          </button>
+          <button className="text-xs hover:text-blue-500 text-right underline">
+            Forgot password?
+          </button>
+          <small className="text-center pt-5">
+            Don't have an account?
+            <Link href="/signup">
+              <a className="text-blue-500 ml-2">Signup</a>
+            </Link>
+          </small>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default login;
---
+++ pages/signup.tsx
@@ -0,0 +1,83 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+
+const signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    cpassword: "",
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <div className="md:h-screen w-screen flex justify-center items-center">
+      <Head>
+        <title>Signup</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-96 bg-white md:shadow-md rounded-md p-6 space-y-10 m-2">
+        <h1 className="font-bold text-4xl text-center">Sign up</h1>
+        <form className="flex flex-col space-y-4">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            onChange={handleChange}
+          />
+          <input
+            className="input-text"
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            onChange={handleChange}
+          />
+          <div className="flex items-center space-x-2 ">
+            <div className="text-gray-400 border p-3 rounded-md bg-gray-100">
+              +91
+            </div>
+            <input
+              className="input-text w-full"
+              type="number"
+              placeholder="Phone number"
+              name="phone"
+              onChange={handleChange}
+            />
+          </div>
+          <input
+            className="input-text"
+            type="password"
+            placeholder="Enter a password"
+            name="password"
+            onChange={handleChange}
+          />
+          <input
+            className="input-text"
+            type="password"
+            placeholder="Confirm password"
+            name="cpassword"
+            onChange={handleChange}
+          />
+          <button className="btn-primary" type="submit" onClick={signup}>
+            Sign up
+          </button>
+          <small className="text-center pt-5">
+            Already have an account?
+            <Link href="/login">
+              <a className="text-blue-500 ml-2">Login</a>
+            </Link>
+          </small>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default signup;
