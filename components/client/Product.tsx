115b6f632c87b498ef13763ed138660962d02d5c
rony thankachan <rony.mail2me@gmail.com>
2022-01-21 04:23:12 +0530

    unsatisfactory product card design

--- components/client/Product.tsx
+++ components/client/Product.tsx
@@ -1,27 +1,24 @@
-import { ShoppingCartIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import Link from "next/link";
 import { ProductType } from "../../types";
 const Product = ({ product }: { product: ProductType }) => {
   return (
-    <div className="border-2 p-2 rounded-md space-y-2 w-fit hover:scale-[102%] transition-all duration-150 ease-out hover:cursor-pointer bg-white">
-      <Link href={"/product/" + product.id}>
-        <a>
-          <Image
-            width={250}
-            height={270}
-            src={product.images[0]}
-            className="object-cover rounded-md"
-          />
-          <p className="text-md text-gray-500 mt-2">{product.title}</p>
-          <p className="text-lg font-semibold">Rs. {product.price}</p>
-          <button className="flex items-center w-full justify-center py-2 border rounded-md space-x-2 bg-gray-300 text-black hover:text-white hover:bg-black transition-all duration-150 ease-in-out">
-            <ShoppingCartIcon className="w-4 h-4" />
-            <small className="text-xs font-semibold">Add to cart</small>
-          </button>
-        </a>
-      </Link>
-    </div>
+    <Link href={"/product/" + product.id}>
+      <a className="bg-[#f7f7f7] shadow-md rounded-md space-y-2 w-fit hover:scale-[102%] transition-all duration-150 ease-out hover:cursor-pointer">
+        <Image
+          width={250}
+          height={270}
+          src={product.images[0]}
+          className="object-cover rounded-md"
+        />
+        <div className="py-4 px-4 bg-white">
+          <p className="text-lg font-thin text-gray-500 mt-2">
+            {product.title}
+          </p>
+          <p className="text-sm font-thin">Rs. {product.price}</p>
+        </div>
+      </a>
+    </Link>
   );
 };
 
