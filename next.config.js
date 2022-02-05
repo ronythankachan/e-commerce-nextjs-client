5a53e00005cc7dd6efebdc4c833c951b6d8937c4
rony thankachan <rony.mail2me@gmail.com>
2022-02-06 03:33:59 +0530

    unwanted image domains removed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: [
-      "assets.ajio.com",
-      "pbs.twimg.com",
-      "c.static-nike.com",
-      "i.pinimg.com",
-      "etimg.etb2bimg.com",
-      "res.cloudinary.com",
-      "product-images-e-commerce.s3.amazonaws.com",
-    ],
+    domains: ["product-images-e-commerce.s3.amazonaws.com"],
   },
   reactStrictMode: true,
 };
