54dc19091512b995c1a5ab22cbe0ea573ddd93cd
rony thankachan <rony.mail2me@gmail.com>
2022-03-01 15:22:07 +0530

    middleware added for all pages

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,19 +1,35 @@
 import jwtDecode from "jwt-decode";
 import { NextURL } from "next/dist/server/web/next-url";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { backendURL } from "../../lib/utils";
+import { backendURL } from "../lib/utils";
+
+const protectedPages: string[] = ["admin", "cart", "orders"];
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const userCookie: string = req.cookies.user;
-  const redirectUrl: NextURL = getRedirectUrl(req);
-  if (userCookie === undefined) return NextResponse.redirect(redirectUrl);
-  else {
-    const accessToken = JSON.parse(userCookie).accessToken;
-    const isAuth = await authorize(accessToken);
-    if (!isAuth) return NextResponse.redirect(redirectUrl);
-    else if (!isAdmin(accessToken)) return NextResponse.redirect("/");
+  if (isProtectedPath(req)) {
+    const userCookie: string = req.cookies.user;
+    const redirectUrl: NextURL = getRedirectUrl(req);
+    if (userCookie === undefined) return NextResponse.redirect(redirectUrl);
+    else {
+      const accessToken = JSON.parse(userCookie).accessToken;
+      const isAuth = await authorize(accessToken);
+      if (!isAuth) return NextResponse.redirect(redirectUrl);
+      else if (isAdminPath(req) && !isAdmin(accessToken))
+        return NextResponse.redirect("/");
+    }
   }
 }
+const isProtectedPath = (req: NextRequest) => {
+  const path = req.nextUrl.pathname;
+  for (let i = 0; i < protectedPages.length; i++) {
+    if (path.includes(protectedPages[i])) return true;
+  }
+  return false;
+};
+
+const isAdminPath = (req: NextRequest) => {
+  return req.nextUrl.pathname.includes("admin");
+};
 
 const getRedirectUrl = (req: NextRequest) => {
   const redirectUrl = req.nextUrl.clone();
